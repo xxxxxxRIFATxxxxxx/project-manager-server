123faf60dc1d218fb25d9a36a2f1605e67250c21
Md. Rifat Islam <xxxxrifatxxxx@gmail.com>
2022-09-16 15:32:58 +0600

    Create Database

--- server.js
+++ server.js
@@ -5,38 +5,7 @@ const http = require("http");
 
 const app = express();
 const server = http.createServer(app);
-const io = require("socket.io")(server);
-
-global.io = io;
-
 const router = jsonServer.router("db.json");
-
-// response middleware
-router.render = (req, res) => {
-    const path = req.path;
-    const method = req.method;
-
-    if (
-        path.includes("/conversations") &&
-        (method === "POST" || method === "PATCH")
-    ) {
-        // emit socket event
-        io.emit("conversation", {
-            data: res.locals.data,
-        });
-    };
-
-    if (
-        path.includes("/messages") && method === "POST"
-    ) {
-        // emit socket event
-        io.emit("message", {
-            data: res.locals.data,
-        });
-    }
-
-    res.json(res.locals.data);
-};
 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
@@ -48,8 +17,8 @@ app.use(middlewares);
 
 const rules = auth.rewriter({
     users: 640,
-    conversations: 660,
-    messages: 660,
+    teams: 660,
+    projects: 660,
 });
 
 app.use(rules);
